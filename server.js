44f0b0731848fdebb1ea160b3c3dbf015fd481e1
Brandon <brandon@reddirtwebdesign.com>
2018-07-11 03:26:39 -0700

    feat: Inital Routes Setup
    Made inital test routes for Users, Posts and Profile. Moved them into thier own file.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
 // DB Config
@@ -10,6 +14,11 @@ const db = require('./config/keys').MONGO_URI;
 mongoose.connect(db).then(() => console.log('Database Connected....'));
 
 app.get('/', (req, res) => res.send('Hello'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
