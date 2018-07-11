594609a202e0dd35d31224e657f4e49b5090b6a7
Brandon <brandon@reddirtwebdesign.com>
2018-07-11 14:55:14 -0700

    feat: Get User Profile Rouute

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-// @route   GET api/profile/test
-// @desc    Test profile route
-// @access  Public
-router.get('/test', (req, res) => {
-  res.json({ msg: 'Profile Works' });
+// Load Profile Modle
+const Profile = require('../../models/Profile');
+// Load User Modle
+const User = require('../../models/User');
+
+// @route   GET api/profile
+// @desc    Get Current Users Profile
+// @access  Private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const errors = {};
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        return res.status(403).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(400).json(err));
 });
 
 module.exports = router;
