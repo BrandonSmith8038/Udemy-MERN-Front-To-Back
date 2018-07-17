b6eebb0b07752f0a9047f68c05d848c971394399
Brandon <brandon@reddirtwebdesign.com>
2018-07-17 01:38:15 -0700

    feat: Login Functionality
    Created login functionality using redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { setCurrentUser } from './actions/authActions';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
 import store from './store';
 
 import Navbar from './components/layout/Navbar';
@@ -10,6 +13,16 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 
 import './App.css';
+
+// Check for Token
+if (localStorage.jwtToken) {
+  // set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get usuer info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
-import { GET_ERRORS } from './types';
 import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from '../utils/setAuthToken';
+
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 // Register User
 
@@ -14,3 +17,34 @@ export const registerUser = (userData, history) => dispatch => {
       })
     );
 };
+
+// Login - Get User Token
+
+export const loginUser = userData => dispatch => {
+  axios
+    .post('/api/users/login', userData)
+    .then(res => {
+      // Save To Local Storage
+      const { token } = res.data;
+      // Set token to local storage
+      localStorage.setItem('jwtToken', token);
+      // Set Token To Auth Header
+      setAuthToken(token);
+      // Decode Token To Get User Data
+      const decoded = jwt_decode(token);
+      // Set Current User
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+// Set Logged In User
+export const setCurrentUser = decoded => ({
+    type: SET_CURRENT_USER,
+    payload: decoded
+  });
