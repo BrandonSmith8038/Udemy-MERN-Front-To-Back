913438ab5f487afa02538f92a0980d89d95fe4f8
Brandon <brandon@reddirtwebdesign.com>
2018-07-17 19:33:32 -0700

    feat: Profile actions component and Delete Account

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
-import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE } from './types';
+import {
+  GET_PROFILE,
+  PROFILE_LOADING,
+  GET_ERRORS,
+  CLEAR_CURRENT_PROFILE,
+  SET_CURRENT_USER,
+} from './types';
 
 // Get Current Profile
 
@@ -43,3 +49,23 @@ export const setProfileLoading = () => ({
 export const clearCurrentProfile = () => ({
   type: CLEAR_CURRENT_PROFILE,
 });
+
+// Delete Account and Profile
+export const deleteAccount = () => dispatch => {
+  if (window.confirm('Are You Sure? This can NOT be undone!')) {
+    axios
+      .delete('/api/profile')
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {},
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data,
+        })
+      );
+  }
+};
