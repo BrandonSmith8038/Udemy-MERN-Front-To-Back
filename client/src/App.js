01091ac4e7f3fe1952ffc21b07d0122e07b33886
Brandon <brandon@reddirtwebdesign.com>
2018-07-17 21:25:03 -0700

    feat: Add Education Form and Functionality

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import PrivateRoute from './components/common/PrivateRoute';
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
 import AddExperience from './components/add-credentials/AddExperience';
+import AddEducation from './components/add-credentials/AddEducation';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -61,6 +62,9 @@ class App extends Component {
               </Switch>
               <Switch>
                 <PrivateRoute exact path="/add-experience" component={AddExperience} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/add-education" component={AddEducation} />
               </Switch>
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
